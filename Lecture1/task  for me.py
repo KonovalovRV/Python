51b38554f5003cbab1f0f6a91a6cda7308d66cf9
Roman <vvptus@yandex.ru>
2022-12-18 03:19:12 +0300

    task8 all

--- Lecture1/task  for me.py
+++ Lecture1/task  for me.py
@@ -1,11 +1,15 @@
-# print('Hello world!')
+# # print('Hello world!')
 
-value=None
-# print(value)
-b=9
-a='Hello \nworld' #вывод на другую строку
-print(a)
+# value=None
+# # print(value)
+# b=9
+# a='Hello \nworld' #вывод на другую строку
+# print(a)
 
-print(b,value,b,a)
+# print(b,value,b,a)
 
-print()
+# print()
+
+f=1<4 or 4>1 
+f= 4==1
+print(f)
--- Lecture1/task8.py
+++ Lecture1/task8.py
@@ -4,3 +4,31 @@
 # Пример:
 # 3 2 4 -> yes
 # 3 2 1 -> no
+
+print('Введите размер шоколада в дольках по длине m= ')
+seg_l = int(input())
+print('Введите размер шоколада в дольках по ширине n= ')
+seg_w = int(input())
+print('Введите количество долек шоколада, которые необходимо отломить k= ')
+seg = int(input())
+
+
+def validate(m, n, k):
+
+    if k >= m*n:
+        return 1
+    if k < n and k < m:
+        return 1
+    else:
+        return k
+
+def quatity_segment_chocolate(mm, nn, kk):
+    if kk % mm == 0 or kk % nn == 0:
+        return '--> yes'
+    else:
+        return '--> no'
+
+segg = validate(seg_l, seg_w, seg)
+
+d=quatity_segment_chocolate(seg_l, seg_w, segg)
+print(d)
