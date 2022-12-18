4766f6ef305cd4853a721a1492194ef3fb0e1da1
Roman <vvptus@yandex.ru>
2022-12-19 00:57:50 +0300

    all task4

--- Lecture1/task4.py
+++ Lecture1/task4.py
@@ -7,3 +7,25 @@
 # 6 -> 1 4 1
 # 24 -> 4 16 4
 # 60 -> 10 40 10
+
+
+birds_petya = 0
+birds_kat = 0
+birds_serg = 0
+
+print('Введите количество журавликов - ->')
+
+value = int(input())
+
+
+def quantity_birds(value):
+
+    birds_serg = value // 6
+    birds_kat = 4 * birds_serg
+    birds_petya = value // 6
+    return (birds_serg,  birds_kat, birds_petya)
+
+
+arg1, arg2, arg3 = quantity_birds(value)
+
+print(f'{arg1} - {arg2} - {arg3}')
